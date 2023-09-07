1a2aab29e17f3e99c1dcaa7eabc8a820204e5775
HARSH KUMAR <142934569+Harsh-Kumar-2003@users.noreply.github.com>
2023-09-07 12:34:35 +0530

    update

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let diceButton = document.getElementById('dice');
 var stringArray = ["red", "green", "yellow", "blue"];
 let i = 0;
 
+
+let redStart = true;
+let yellowStart = true;
+let greenStart = true;
+let blueStart = true;
+
+let result=0;
+
 //Generating a random number
 function rollDice() {
     let randomNumber = Math.random();
@@ -12,9 +20,45 @@ function rollDice() {
 
 // Add an event listener to the dice button (Rolling the dice)
 diceButton.addEventListener('click', () => {
-    const result = rollDice();
+    result = rollDice();
     diceButton.innerText = result;
     diceButton.style.backgroundColor = stringArray[i];
+
+    if (i == 0)
+        redTurn();
+    else if (i == 1)
+        greenTurn();
+    else if (i == 3)
+        yellowTurn();
+    else
+        blueTurn();
+
     if( result != 6)
         i = (i + 1) % 4;
+
+
 });
+
+function redTurn()
+{
+    if (redStart)
+    {
+        if (i == 6)
+                
+    }
+}
+
+function yellowTurn()
+{
+
+}
+
+function greenTurn()
+{
+
+}
+
+function blueTurn()
+{
+
+}
