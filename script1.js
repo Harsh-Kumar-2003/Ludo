5a626940080a43fb704c9092a4b56b17c6c3668c
sumer27feb <sumerdevsingh@gmail.com>
2023-09-07 15:19:55 +0530

    Numbering the path

--- script1.js
+++ script1.js
@@ -1,28 +1,32 @@
 console.log("Welcome Lodu!");
 let diceButton = document.getElementById('dice');
-let msgBox = document.getElementsByClassName('message');
-var stringArray = ["red", "green", "yellow", "blue"];
+let msgBox = document.getElementById('message');
+var player = ["red", "green", "yellow", "blue"];
 let i = 0;
 
 //Player turn 
 function startGame() {
-    if (diceButton.style.backgroundColor == blueviolet) {
-        msgBox.innerText = "Press roll to start the game";
-
-    }
+    msgBox.innerText = "RED start the game";
 }
 
 //Generating a random number
 function rollDice() {
-    let randomNumber = Math.random();
-    randomNumber = Math.floor(randomNumber * 6) + 1;
-    return randomNumber;
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    diceButton.innerText = randomNumber;
+    diceButton.style.backgroundColor = player[i];
+
+    if (randomNumber != 6) {
+        i = (i + 1) % 4;
+        msgBox.innerText = player[i].toUpperCase() + " roll the dice!"
+    }
 }
 
 // Add an event listener to the dice button (Rolling the dice)
 diceButton.addEventListener('click', () => {
-    const result = rollDice();
-    diceButton.innerText = result;
-    diceButton.style.backgroundColor = stringArray[i];
-    i = (i + 1) % 4;
+    rollDice();
+
+    //Changing turns
+
 });
+
+window.onload = startGame;
